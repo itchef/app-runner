f69bf05da7b6d498b19d3f76717e0b96df7c7a63
Kaustav Chakraborty <kaustav.ron@gmail.com>
2018-06-24 18:16:24 +0530

    Kaustav | #44 | Modified write to json file as stringify version | TG-44

--- configModifier.js
+++ configModifier.js
@@ -2,8 +2,9 @@ const fs =      require('fs');
 const config =  require('./dist/config/config.json');
 
 var updateAppAPI = function(apiPath) {
-    config.apiURL = apiPath.toString()
-    fs.writeFile('./dist/config/config.json', config, 'utf8')
+    config.apiURL = apiPath.toString();
+    console.log(config.apiURL);
+    fs.writeFile('./dist/config/config.json', JSON.stringify(config), 'utf8')
 };
 
 if (process.env.API_PATH)
